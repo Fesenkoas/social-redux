a28387ae2ef83cbe0984fb9910b0ce94c71b66bf
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-10-20 19:59:47 +0300

    Update Setting.jsx

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector, useCallback } from "react-redux";
+import React, { useEffect, useState, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getProfileFetch,
   getStatusFetch,
