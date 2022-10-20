bde9386d295baea5fa98f547eea9161428fc1df2
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-10-20 19:37:49 +0300

    Update getUsersFetch.js

--- src/future/action/getUsersFetch.js
+++ src/future/action/getUsersFetch.js
@@ -53,8 +53,8 @@ export const deleteFollowFetch = (id) => (dispatch) => {
 };
 //get all list users
 export const getFollowFetch = () => (dispatch) => {
-  let pagesCount;
-  const allUser=[]
+//   let pagesCount;
+//   const allUser=[]
   fetch(`${baseURL}users`, instanseGet)
     .then((res) => res.json())
     .then((data) =>dispatch(getFollowed(data)));
